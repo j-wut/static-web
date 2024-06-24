9ac6b007e0ac4d194d920ce9cae71693f8ff91a1
j-wut <james.wut@gmail.com>
2024-06-24 00:27:38 -0400

    added LeafNode and accompanying unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,3 +12,15 @@ class HTMLNode:
         return "".join([f' {k}="{v}"' for k,v in self.props.items()])
     def __repr__(self):
         return {"tag": self.tag, "value":self.value, "children": self.children, "props": self.props}
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag = None, value = None, props = {}):
+        super().__init__(tag, value, [], props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError()
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,11 +1,17 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         assert node.props_to_html() == ' href="https://www.google.com" target="_blank"'
+
+    def test_leaf_node_to_html(self):
+        node1 = LeafNode("p", "This is a paragraph of text.")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        assert node1.to_html() == '<p>This is a paragraph of text.</p>'
+        assert node2.to_html() == '<a href="https://www.google.com">Click me!</a>'
 
 if __name__ == "__main__":
     unittest.main()
